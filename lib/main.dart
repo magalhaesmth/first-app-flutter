314dc652638235dceeaedc196819dd4f623c4125
Matheus Magalhães Urbano <89428438+magalhaesmth@users.noreply.github.com>
2022-07-12 20:09:05 -0300

    Main atualizada projetin novo

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/view/calculadora.dart';
-import 'package:flutter_app/view/calcular_idade.dart';
-import 'package:flutter_app/view/hello_word.dart';
-import 'package:flutter_app/view/menu.dart';
+import 'package:flutter_app/widget/tarefa_form.dart';
+import 'package:flutter_app/widget/usuario.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,64 +9,18 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(primarySwatch: Colors.pink),
-        routes: {
-          "/": (context) => Menu(),
-          "/calcularIdade": (context) => CalcularIdade(),
-          "/helloWord": (context) => HelloWord(),
-          "/calculadora": (context) => Calculadora()
-        });
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      routes: {
+        "/": (context) => Usuario(),
+        "/tarefaForm": (context) => TarefaForm()
+      },
     );
   }
 }
